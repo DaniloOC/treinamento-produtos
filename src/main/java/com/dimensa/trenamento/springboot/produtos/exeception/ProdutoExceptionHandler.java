0b39c650acaf69838314134243eeb4d50e8b2097
Danilo Cruz <do.cruz@dimensa.com.br>
2024-02-19 15:35:18 -0300

    Treinamento Java + API  concluido

--- src/main/java/com/dimensa/trenamento/springboot/produtos/exeception/ProdutoExceptionHandler.java
+++ src/main/java/com/dimensa/trenamento/springboot/produtos/exeception/ProdutoExceptionHandler.java
@@ -1,6 +1,14 @@
 package com.dimensa.trenamento.springboot.produtos.exeception;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +22,20 @@ public class ProdutoExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String produtoNotFoundException(ProdutoNotFoundException exception) {
         return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<Map<String, List<String>>> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<String> errors = exception.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).toList();
+        return new ResponseEntity<>(errorsMap(errors), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> errorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("erros", errors);
+        return errorResponse;
     }
     
 }
--- src/main/java/com/dimensa/trenamento/springboot/produtos/service/ProdutoService.java
+++ src/main/java/com/dimensa/trenamento/springboot/produtos/service/ProdutoService.java
@@ -3,8 +3,13 @@ package com.dimensa.trenamento.springboot.produtos.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dimensa.trenamento.springboot.produtos.dto.ProdutoAtualizaDTO;
+import com.dimensa.trenamento.springboot.produtos.exeception.ProdutoNotFoundException;
 import com.dimensa.trenamento.springboot.produtos.model.Produto;
 import com.dimensa.trenamento.springboot.produtos.repository.ProdutoRepository;
 
@@ -17,8 +22,8 @@ public class ProdutoService {
         this.repository = repository;
     }
 
-    public List<Produto> listaTodos() {
-        return repository.findAll();
+    public Page<Produto> listaTodos(Pageable paginacao) {
+        return repository.findAll(paginacao);
     }
 
     public Produto incluir(Produto produto) {
@@ -27,6 +32,22 @@ public class ProdutoService {
 
     public Optional<Produto> obter(Long id) {
         return repository.findById(id);
+    }
+
+    @Transactional
+    public Produto atualizar(Long id, ProdutoAtualizaDTO dto) {
+        Optional<Produto> produtoOpt = repository.findById(id);
+        if (!produtoOpt.isPresent()) {
+            throw new ProdutoNotFoundException(id);
+        }
+        Produto produto = produtoOpt.get();
+        produto.atualiza(dto);
+        return repository.save(produto);
+    }
+
+    @Transactional
+    public void excluir(Long id) {
+        repository.deleteById(id);
     }
 
 }
